0df819b6daa3dc57d03a87a25922d85759ce3556
Avik219 <nitantbhagat@gmail.com>
2023-03-04 12:45:36 +0530

    7th Commit 4/3/23

--- app/src/main/java/com/travelease/nitant/HomeActivity.java
+++ app/src/main/java/com/travelease/nitant/HomeActivity.java
@@ -1,9 +1,13 @@
 package com.travelease.nitant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,6 +15,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.travelease.nitant.databinding.ActivityHomeBinding;
+
+import java.util.zip.Inflater;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -34,4 +40,32 @@ public class HomeActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_right_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.menu_settings)
+        {
+
+        } else if (item.getItemId()==R.id.menu_logout) {
+            Intent i = new Intent(HomeActivity.this,LoginActivity.class);
+            startActivity(i);
+            finish();
+        }
+//        switch (item){
+//            case R.id.menu_settings:
+//
+//                break;
+//            case R.id.menu_logout:
+//
+//                break;
+//            case
+//        };
+
+        return super.onOptionsItemSelected(item);
+    }
 }
